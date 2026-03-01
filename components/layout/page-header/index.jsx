093d052449b5e0c3c2f7a9603ce52bb907589ba5
victor-prm <victorpabb@gmail.com>
2026-03-01 17:11:35 +0100

    chore: change header text

--- components/layout/page-header/index.jsx
+++ components/layout/page-header/index.jsx
@@ -1,13 +1,18 @@
 export default function PageHeader() {
+
+
+
+
     return (
         <header className="cust-content-container text-black text-xl flex flex-col gap-2">
             <h1 className="font-ibm-serif font-medium text-6xl sm:text-8xl md:text-9xl leading-none mb-8">
                 Hello
                 <span className="block">I'm Victor</span>
             </h1>
-            <p>I'm a web developer based in Copenhagen.</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia harum accusantium optio.</p>
-            <p>I'm also a co-founder and design technologist in Circuit Circus, an interaction design studio based in copenhagen</p>
+            <p>I'm a web developer, with a background in interaction design</p>
+            <p>I thrive in frontend work, but have great understanding of backend tasks as well</p>
+            <p>I currently work at Fluxscape as a Frontend Developer</p>
+            <p>I'm also a co-founder and design technologist in Circuit Circus, an interaction design studio based in Copenhagen</p>
         </header>
     )
-}
+} 
