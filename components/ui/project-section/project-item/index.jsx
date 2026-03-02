29a1b0cec6a8c0e61aead9c20c446d8f8f67a7f5
victor-prm <victorpabb@gmail.com>
2026-03-02 15:33:06 +0100

    style: add hover states to proejct cards

--- components/ui/project-section/project-item/index.jsx
+++ components/ui/project-section/project-item/index.jsx
@@ -5,26 +5,42 @@ export default function ProjectItem({
   ...props
 }) {
   return (
-    <li className="flex w-full">
-      {props.thumbnail && (
-        <Image
-          src={props.thumbnail}
-          alt={props.title}
-          width={240}
-          height={240}
-          className="aspect-video object-cover object-top w-full max-w-1/2 rounded-xl ring-2 ring-gray-50"
-          priority={false}
-        />
+    <li className="w-full">
+      <Link
+        href={`/projects/${props.slug}`}
+        className="group flex w-full relative"
+      >
+        {props.thumbnail && (
+          <figure
+            className="
+          relative max-w-1/2 rounded-xl overflow-hidden
+          ring-2 ring-gray-50
+          grayscale-100 transition
+          group-hover:grayscale-0
+          after:absolute after:inset-0 after:bg-black/50 after:z-10
+          after:transition
+          group-hover:after:bg-black/0
+        "
+          >
+            <Image
+              src={props.thumbnail}
+              alt={props.title}
+              width={240}
+              height={240}
+              className="aspect-video object-cover object-top"
+            />
+          </figure>
+        )}
 
-      )}
-      <hgroup className="p-2 text-shadow-2xs text-shadow-gray-500/20">
-        <Link href={`/projects/${props.slug}`}>
-          <h2 className="text-xl font-medium">{props.title}</h2>
-        </Link>
-
-        <p className="text-sm md:text-base font-ibm-mono font-light w-24 opacity-75 shrink-0">{props.year}</p>
-      </hgroup>
-
+        <hgroup className="p-2">
+          <h2 className="text-xl font-medium">
+            {props.title}
+          </h2>
+          <p className="text-sm md:text-base font-ibm-mono font-light opacity-75">
+            {props.year}
+          </p>
+        </hgroup>
+      </Link>
     </li>
   );
 }
